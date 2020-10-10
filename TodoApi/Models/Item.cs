2e7d8de132fd36adf7f95c74958e8b916b40669f
Kelvin Lieu <kelvinlieu12345@gmail.com>
2020-10-10 11:37:58 +0800

    first commit

---
+++ TodoApi/Models/Item.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoApi.Models
+{
+    public class Item
+    {
+        public long id { get; set; }
+        public string name { get; set; }
+        public bool done { get; set; }
+    }
+}
